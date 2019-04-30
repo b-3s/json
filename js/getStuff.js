f4f6e85993fa28e82f8b5339efe83217286799ed
b3s <b.schmidleitner@gmail.com>
2019-04-30 10:58:08 +0200

    database and index.html stuff

--- js/getStuff.js
+++ js/getStuff.js
@@ -8,14 +8,17 @@ $(document).ready(function(){
 		$.getJSON( "js/theJson.json", function( json ) {
 		    console.log( "JSON Data received, hobby is " + json.hobby);
 		    $("#jsonStuff").html("<h1>attached via .html method</h1>");
-		    $("#scndJsonStuff").html(json.pets[0].name + "</br>");
+		    $("#scndJsonStuff").append(json.pets[0].name + "</br>");
 		    $("#scndJsonStuff").append(json.name + " is " +
 		    	json.age + " years old.");
 		});
 	});
 
-	$("#dbAction").click(function(){
-		$("#demo").load("php/db_action.php");
+	$("#getDbAction").click(function(){
+		if($("#loadedDbStuff").length === 0){
+			$("#dbStuff").append("<div id=\"loadedDbStuff\"></div>");
+		}
+		$("#loadedDbStuff").load("php/db_action.php");
 	});
 	
 });
